62712264b3a2ae5f463035a87421303ef57d30da
ST-GuY <estebanlvy23@gmail.com>
2025-02-18 10:01:32 +0100

    Hi

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,16 @@
 
 	printf("%d ", n);
 	if (n > 0)
-    {
+{
         printf("is positive\n");
-    }
+}
 	else if (n == 0)
-    {
+{
         printf("is zero\n");
-    }
+}
 	else
-    {
+{
         printf("is negative\n");
-    }
+}
 	return (0);
 }
