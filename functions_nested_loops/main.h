7b06bc70cd97ddcb184d7d307e40ae15417e1064
ST-GuY <estebanlvy23@gmail.com>
2025-02-19 10:38:02 +0100

    hi

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes characters c to stdout
+ * @c: character to print
+ * Return: On Success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
