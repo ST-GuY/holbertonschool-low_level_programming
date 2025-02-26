6d64dce176ae25774cc883eb0315c6dcb25cbfd4
ST-GuY <estebanlvy23@gmail.com>
2025-02-26 11:18:20 +0100

    hi

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Dessine une ligne droite dans le terminal.
+ * @n: Nombre de fois que le caractère '_' doit être imprimé.
+ *
+ * Description :
+ * Cette fonction affiche une ligne horizontale composée du caractère '_'.
+ * Si n est inférieur ou égal à 0, seul un retour à la ligne est imprimé.
+ */
+void print_line(int n)
+{
+    int i;
+
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+            _putchar('_');
+    }
+    _putchar('\n');
+}
+
