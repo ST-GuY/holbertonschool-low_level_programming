fe1211426eac8eaacb3c583be3c026daf56eb0b9
ST-GuY <estebanlvy23@gmail.com>
2025-02-27 14:42:05 +0100

    hh

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes characters c to stdout
+ * @c: character to print
+ * Return: On Success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n);
