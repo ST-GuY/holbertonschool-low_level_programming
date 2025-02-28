09a5da0cd4612c39e44a840167b3e89d370811e7
ST-GuY <estebanlvy23@gmail.com>
2025-02-28 17:10:54 +0100

    hi

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,29 +2,28 @@
 
 /**
  * _strcat - Concatenates two strings.
- * @dest: The destination string, which must be large enough to hold the result.
+ * @dest: The destination string,
+ * which must be large enough to hold the result.
  * @src: The source string to append to dest.
- *
  * Return: Pointer to the destination string (dest).
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    /* Find the end of dest */
-    while (dest[i] != '\0')
-        i++;
 
-    /* Append src to dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (dest[i] != '\0')
+	i++;
 
-    /* Add the null terminator */
-    dest[i] = '\0';
 
-    return dest;
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
