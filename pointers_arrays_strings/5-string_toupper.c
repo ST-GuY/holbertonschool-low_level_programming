3426ec256ec2956122ff5e328407609d5d587072
ST-GuY <estebanlvy23@gmail.com>
2025-03-01 12:38:00 +0100

    hi

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @str: The string to modify.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	/* Parcourir la chaîne et convertir les lettres minuscules en majuscules */
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - ('a' - 'A');
+		i++;
+	}
+
+	return (str);
+}
