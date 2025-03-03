973ba9bff436937f3d3540fc1bfc3c837c3b87ce
ST-GuY <estebanlvy23@gmail.com>
2025-03-03 11:17:41 +0100

    x2

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,13 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    /* Parcourir les n premiers octets de la zone mémoire */
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b; /* Remplir chaque octet avec la valeur b */
-    }
+	/* Parcourir les n premiers octets de la zone mémoire */
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b; /* Remplir chaque octet avec la valeur b */
+	}
 
-    return (s); /* Retourner le pointeur s */
+	return (s); /* Retourner le pointeur s */
 }
