8f90e7e23399d41abd6fea9fa49a27c3508e1571
ST-GuY <estebanlvy23@gmail.com>
2025-03-05 15:07:28 +0100

    x2

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -26,9 +26,7 @@ int _is_prime_helper(int n, int i)
 
 int is_prime_number(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (1);
+	if (n <= 1)
+		return (0);
 	return (_is_prime_helper(n, 2));
 }
