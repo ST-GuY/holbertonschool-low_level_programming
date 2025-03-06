edd18bed7bad50cf85e86e221354aed672931b66
ST-GuY <estebanlvy23@gmail.com>
2025-03-06 16:34:55 +0100

    brain

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+		return (0);
+}
