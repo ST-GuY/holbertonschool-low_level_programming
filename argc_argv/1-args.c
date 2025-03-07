3bd803b748661df71d2e1742d9e8bec30275097a
ST-GuY <estebanlvy23@gmail.com>
2025-03-07 15:30:40 +0100

    hi

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-/* main - Prints the name of the program
+/**
+ * main - Prints the name of the program
  * @argc: Argument count
  * @argv: Argument vector
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(int argc, char *argv[])
 
 {
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	(void)argv;
 
 	return (0);
