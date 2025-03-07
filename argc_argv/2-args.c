65c4a609f2b0083196c3bd02b241168b68395ea6
ST-GuY <estebanlvy23@gmail.com>
2025-03-07 16:06:13 +0100

    hi

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -10,9 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-for (int i = 0; i < argc; i++)
+int i;
+for (i = 0; i < argc; i++)
 	{
-	printf("%d\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
