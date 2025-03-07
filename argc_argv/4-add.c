fddb027c91fcddffb488f86aa4c94650e028f1e6
ST-GuY <estebanlvy23@gmail.com>
2025-03-07 20:05:32 +0100

    hi

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * 
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	result = 0;
+	printf("%d\n", result);
+	if (argc == 1)
+		printf("0\n");
+	for (j = 
+	for (i = 0; i < strlen(argv[1]); ++i);
+	{
+		if (argv[1][i] < '0' || argv[1][i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	return (0);
+}
