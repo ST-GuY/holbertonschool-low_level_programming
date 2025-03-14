2d78060fe68cbad43b29df8a5486d9eabfc222b1
ST-GuY <estebanlvy23@gmail.com>
2025-03-14 22:11:50 +0100

    hi

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,14 +10,19 @@
  * @age: Âge du chien (nombre à virgule flottante)
  * @owner: Nom du propriétaire du chien (chaîne de caractères)
  */
-typedef struct dog {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+	typedef struct dog dog_t;
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+
 #endif /* DOG_H */
 
