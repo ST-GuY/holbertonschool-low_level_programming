9e37653cda5f6eb507f16160797bebb0cc1cadd4
ST-GuY <estebanlvy23@gmail.com>
2025-03-14 20:51:33 +0100

    hi

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,33 +11,36 @@
  * Return: Un pointeur vers le chien crée, ou NULL si l'échec.
  */
 
-dog_t *new_dog(char *name, float age, char *owner) {
+dog_t *new_dog(char *name, float age, char *owner)
+{
 	dog_t *dog;
 	int name_len, owner_len;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return NULL;
+	return (NULL);
 
 	name_len = strlen(name) + 1;
 	owner_len = strlen(owner) + 1;
 
 	dog->name = malloc(name_len);
-	if (dog->name == NULL) {
+	if (dog->name == NULL)
+	{
 		free(dog);
-		return NULL;
+	return (NULL);
 	}
 
 	dog->owner = malloc(owner_len);
-	if (dog->owner == NULL) {
+	if (dog->owner == NULL)
+	{
 		free(dog->name);
 		free(dog);
-		return NULL;
+	return (NULL);
 	}
 
 	strcpy(dog->name, name);
 	strcpy(dog->owner, owner);
 	dog->age = age;
 
-	return dog;
+	return (dog);
 }
