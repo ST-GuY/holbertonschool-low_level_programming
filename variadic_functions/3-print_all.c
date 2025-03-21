b2b4f9e1fb0877c31b59803568dc795f55f13941
ST-GuY <estebanlvy23@gmail.com>
2025-03-21 09:14:42 +0100

    hi

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
- * 
- * 
+ * print_all - Imprime différents types de données en fonction du format donné.
+ * @format: Liste des types d'arguments passés à la fonction.
+ *
+ * Description: c = char, i = int, f = float, s = string.
+ * Si une chaîne est NULL, elle est remplacée par "(nil)".
+ * Les types non reconnus sont ignorés.
  */
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str, *sep = "";
 
-va_list args;
-va_start(args, format);
+	va_start(args, format);
 
-int i = 0;
-while (format[i] != '\0')
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
+		{
+			printf("%s", sep);
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+			}
+			sep = ", ";
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
