9ddb34bc4c5fe67f8b7dbd81da12d48b880f3a73
ST-GuY <estebanlvy23@gmail.com>
2025-04-02 09:29:15 +0200

    add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,45 +1,39 @@
 #include "lists.h"
 
+/* Definition of the list_t structure */
+typedef struct list_s
+{
+    char *str;
+    struct list_s *next;
+} list_t;
+
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
- * add_node_end - Adds a new node at the end of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the new node.
- * Return: The address of the new element, or NULL if it failed.
+ * @head: Pointer to the pointer of the first node of the list.
+ * @str: String to be duplicated and added to the new node.
+ *
+ * Return: Address of the new element, or NULL if it fails.
  */
-
-list_t *add_node_end(list_t **head, const char *str)
-
+list_t *add_node(list_t **head, const char *str)
 {
-
-	list_t *new_node;
-
-	if (str == NULL)
-		return (NULL);
-
-new_node = malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
-
+    list_t *new_node;
+    
+    if (str == NULL)
+        return (NULL);
+    
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+    
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    
+    new_node->next = *head;
+    *head = new_node;
+    
+    return (new_node);
 }
