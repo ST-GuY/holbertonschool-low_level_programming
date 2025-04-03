ab54e78065587043db2ece30e2ce7cbbf5ae7337
ST-GuY <estebanlvy23@gmail.com>
2025-04-03 18:57:50 +0200

    hi

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,62 +3,65 @@
 #include <string.h>
 
 /**
- * _strlen - Calculates the length of a string
- * @str: The input string
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: A pointer to the pointer to the first node
+ * @str: The string to be stored in the new node (duplicated)
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+	char *dup_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = dup_str;
+	new_node->len = _strlen(dup_str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: The string to measure
  *
  * Return: The length of the string
  */
 int _strlen(const char *str)
 {
-int len = 0;
+	int len = 0;
 
-while (str[len])
-len++;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-return (len);
+	return (len);
 }
 
-/**
- * add_node_end - Adds a new node at the end of a linked list
- * @head: A pointer to the pointer to the first node
- * @str: The string to be stored in the new node
- *
- * Return: Address of the new node, or NULL if it fails
- */
-
-list_t *add_node_end(list_t **head, const char *str)
-
-{
-list_t *new_node, *temp;
-
-if (!str)
-return (NULL);
-
-new_node = malloc(sizeof(list_t));
-if (!new_node)
-return (NULL);
-
-new_node->str = strdup(str);
-if (!new_node->str)
-{
-free(new_node);
-return (NULL);
-}
-
-new_node->len = _strlen(new_node->str);
-new_node->next = NULL;
-
-if (*head == NULL)
-{
-*head = new_node;
-}
-else
-{
-temp = *head;
-while (temp->next)
-temp = temp->next;
-
-temp->next = new_node;
-
-return (new_node);
-}
