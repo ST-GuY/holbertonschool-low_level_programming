16cd2b866b3e82c734029c74c7a099153be8b59f
ST-GuY <estebanlvy23@gmail.com>
2025-04-03 19:10:21 +0200

    hi

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - Affiche tous les éléments d'une liste dlistint_t
+ * @h: Pointeur vers la tête de la liste
+ *
+ * Return: Nombre de nœuds dans la liste
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
