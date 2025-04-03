97c5c02f787598bbf923a87364af8fe6baf78da0
ST-GuY <estebanlvy23@gmail.com>
2025-04-03 23:04:56 +0200

    hi

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,19 @@
-#include <stddef.h>
 #include "lists.h"
 
 /**
- * dlistint_len - Compte le nombre de nœuds dans une liste doublement chaînée.
- * @h: Pointeur vers la tête de la liste.
- *
- * Retourne: Le nombre de nœuds dans la liste.
+ * dlistint_len - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
