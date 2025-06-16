1e50b57497c3f99b6fca9270bafa2038aea8b404
ST-GuY <estebanlvy23@gmail.com>
2025-06-16 14:06:36 +0200

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the phrase
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line, using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
