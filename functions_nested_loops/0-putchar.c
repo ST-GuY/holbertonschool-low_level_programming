4df705bb6b8d4a8c5d0e2092300985797866b24a
ST-GuY <estebanlvy23@gmail.com>
2025-06-17 14:26:16 +0200

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "_putchar" followed by a new line
+ * to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}
