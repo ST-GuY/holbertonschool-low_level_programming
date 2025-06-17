98f167e50474d22d612f174d8455ee5c8ec76f67
ST-GuY <estebanlvy23@gmail.com>
2025-06-17 11:56:01 +0200

    print

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the numbers from 0 to 9 separated by ", ",
+ * using only the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
