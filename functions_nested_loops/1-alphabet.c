4c4f134e8bd3bf7f702f1beba8feb1ccc8623110
ST-GuY <estebanlvy23@gmail.com>
2025-06-17 14:54:03 +0200

    al

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 
 /**
- * print_alphabet - Affiche les lettres minuscules de a à z, suivies d’un saut de ligne.
+ * print_alphabet - Affiche les lettres minuscules de a à z,
+ *  suivies d’un saut de ligne.
  *
  * Return: Rien (void)
  */
 
 void print_alphabet(void)
 {
-	int al;
-	for (al = 'a'; al <= 'z'; al++)
+int al;
+for (al = 'a'; al <= 'z'; al++)
 	_putchar(al);
 	_putchar('\n');
 }
