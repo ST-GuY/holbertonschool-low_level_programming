107e7fad5f4498beaff28a5c502f6f64a5923972
ST-GuY <estebanlvy23@gmail.com>
2025-06-18 09:50:00 +0200

    alph

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_alphabet - Affiche les lettres minuscules de a à z,
+ * suivies d’un saut de ligne.
+ * dix fois
+ * Return: Rien (void)
+ */
+
+void print_alphabet_x10(void)
+{
+int al;
+int i;
+for ( i = 0; i <= 10; i++)
+{
+for (al = 'a'; al <= 'z'; al++)
+{
+	_putchar(al);
+}
+_putchar('\n');
+}
+}
