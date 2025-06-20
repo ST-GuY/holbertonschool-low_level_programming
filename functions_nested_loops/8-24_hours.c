e757da24b47c99e43674e4a8252c01ea58a9d7a8
ST-GuY <estebanlvy23@gmail.com>
2025-06-20 09:27:10 +0200

    jackbauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - Affiche chaque minute de la journée de 00:00 à 23:59.
+ *
+ * Description:
+ * Cette fonction utilise deux boucles imbriquées pour afficher toutes
+ * les combinaisons d'heures et de minutes dans une journée entière,
+ * au format HH:MM. Chaque minute est affichée sur une nouvelle ligne.
+ *
+ * Return: Rien (void)
+ */
+
+void jack_bauer(void)
+{
+	int h, min;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+		printf("%02d:%02d\n", h, min);
+		}
+	}
+}
