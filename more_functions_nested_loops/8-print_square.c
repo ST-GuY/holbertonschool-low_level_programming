393d1c602e6932ca309bd4495ed8a14ff4b2f08f
ST-GuY <estebanlvy23@gmail.com>
2025-06-23 14:53:33 +0200

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * print_square - Affiche un carré de taille donnée avec le caractère '#'
+ * @size: La taille du carré (hauteur et largeur)
+ *
+ * Description: Cette fonction imprime un carré
+ * composé de caractères '#'
+ * avec un nombre de lignes et de colonnes égal à la valeur de `size`.
+ * Si `size` est inférieur ou égal à 0, elle imprime
+ * simplement une nouvelle ligne.
+ */
 
 void print_square(int size)
 {
