6d47fa0f94b159dc6c43c0f86fb6b13792bdec06
ST-GuY <estebanlvy23@gmail.com>
2025-06-23 14:28:29 +0200

    print

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-
+/**
+ * print_diagonal - Dessine une diagonale sur la console
+ * @n: Le nombre de fois que le caractère '\' doit être imprimé
+ *
+ * Description: Pour chaque ligne, cette fonction imprime un certain
+ * nombre d'espaces suivi du caractère '\', formant ainsi une diagonale.
+ * Si n est inférieur ou égal à 0, la fonction imprime simplement une nouvelle ligne.
+ */
 
 void print_diagonal(int n)
 {
