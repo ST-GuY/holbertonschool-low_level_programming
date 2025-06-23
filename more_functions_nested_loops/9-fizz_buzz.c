3d60e0090d989400069dde79c10c7ec073b8c8f8
ST-GuY <estebanlvy23@gmail.com>
2025-06-23 16:03:47 +0200

    betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+		for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+			if (i % 3 == 0 && i % 5 == 0)
 			{
 				printf("FizzBuzz ");
 			}
-			else if ( i % 3 == 0)
+				else if (i % 3 == 0)
 				{
 					printf("Fizz ");
 				}
@@ -34,7 +34,7 @@ int main(void)
 					else
 					{
 						printf("%d", i);
-						printf(" ");	
+						printf(" ");
 					}
 	}
 	printf("\n");
