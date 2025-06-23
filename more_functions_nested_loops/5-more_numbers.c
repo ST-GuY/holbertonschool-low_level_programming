6d8b65dfdcc9c03f7af41f31872478f9a4e8c939
ST-GuY <estebanlvy23@gmail.com>
2025-06-23 11:49:21 +0200

    more

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,8 +4,10 @@
  * more_numbers - Affiche 10 fois les nombres de 0 à 14.
  *
  * Description : Cette fonction utilise deux boucles imbriquées pour afficher
- * les nombres de 0 à 14, dix fois de suite. Si le nombre est supérieur ou égal
- * à 10, elle imprime d'abord le chiffre des dizaines, suivi du chiffre des unités.
+ * les nombres de 0 à 14, dix fois de suite.
+ * Si le nombre est supérieur ou égal
+ * à 10, elle imprime d'abord le chiffre des dizaines,
+ * suivi du chiffre des unités.
  * Chaque série de 0 à 14 est suivie d’un saut de ligne.
  *
  * Retour : Rien (void).
@@ -16,7 +18,7 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 0; a < 10; a++)
+		for (a = 0; a < 10; a++)
 		{
 			for (b = 0; b <= 14; b++)
 			{
