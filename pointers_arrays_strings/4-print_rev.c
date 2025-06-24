dc3322fc5619da9b89cfaf75e94537bd3e38d8cb
ST-GuY <estebanlvy23@gmail.com>
2025-06-24 16:18:13 +0200

    rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,26 @@
  * Description: This function takes a string as input and
  * prints its characters in reverse order using a loop.
  */
+int _strlen(char *s)
+{
+	char *p;
+
+	if (s == NULL)
+		return (0);
+
+	p = s;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+		return (p - s);
+}
 
 void print_rev(char *s)
 {
 	int i;
-    i = strlen(s) - 1;
+    i = _strlen(s) - 1;
 
 	while (i >= 0)
 	{
