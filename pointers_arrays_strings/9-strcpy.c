6386fb118999e26a2dcea213424ef12af2835345
ST-GuY <estebanlvy23@gmail.com>
2025-06-25 15:56:41 +0200

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,14 +4,7 @@
  * _strcpy - Copie la chaîne de caractères pointée par src vers dest
  * @dest : Pointeur vers le tampon de destination
  * @src : Pointeur vers la chaîne source à copier
- *
- * Description : Cette fonction copie la chaîne de caractères de src
- * (y compris l'octet nul de fin “\N”) dans le tampon pointé par src.
- * l'octet nul de fin “\0”) dans le tampon pointé par dest.
- * Elle suppose que dest dispose de
- * suffisamment d'espace pour contenir la chaîne.
- *
- * Retour : Un pointeur sur dest
+ * Return : Pointeur vers dest
  */
 
 char *_strcpy(char *dest, char *src)
