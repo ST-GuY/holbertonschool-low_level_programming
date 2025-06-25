6d85a4fa4c3fbdbe3fcf1370651b39e661607074
ST-GuY <estebanlvy23@gmail.com>
2025-06-25 22:20:42 +0200

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatène deux chaînes de caractères
+ * @dest: Chaîne de destination, doit être assez grande pour contenir le résultat
+ * @src: Chaîne source à ajouter à la fin de dest
+ *
+ * Return: Pointeur sur la chaîne résultante (dest)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0; /* Index pour parcourir dest */
+	int j = 0; /* Index pour parcourir src */
+
+	/* Trouver la fin de la chaîne dest (juste avant le '\0') */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Copier les caractères de src dans dest à partir de la fin */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Ajouter le caractère nul de fin à la nouvelle chaîne */
+	dest[i] = '\0';
+
+	/* Retourner le pointeur vers dest */
+	return (dest);
+}
