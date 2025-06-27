f0fff21a9c173d63a8af977a7141051cdb638833
ST-GuY <estebanlvy23@gmail.com>
2025-06-27 09:50:00 +0200

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,27 @@
 #include "main.h"
 
 /**
- * _strncpy - Copie au plus n caractères de la chaîne src vers dest
+ * _strncpy - Copie au plus n caractères d'une chaîne src dans dest
  * @dest: pointeur vers la chaîne de destination
  * @src: pointeur vers la chaîne source
- * @n: nombre maximal de caractères à copier
+ * @n: nombre maximum de caractères à copier
  *
  * Return: pointeur vers la chaîne de destination (dest)
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i; /* Variable utilisée pour parcourir les caractères */
+	int i; /* Variable pour l'index de la boucle */
 
-	/* Copier les caractères de src dans dest tant que i < n et que src[i] n'est pas le caractère nul */
+	/* Copier les caractères de src vers dest tant qu'on*/
+	/*est dans la limite n et que src[i] n'est pas '\0' */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	/* Si src est plus courte que n, ajouter un seul caractère nul à la suite */
-	if (i < n)
-	{
-		i++;
+	/* Remplir le reste de dest avec '\0' si src est plus courte que n */
+	for (; i < n; i++)
 		dest[i] = '\0';
-	}
 
-	/* Retourne le pointeur vers la chaîne destination */
+	/* Retourner le pointeur vers la chaîne de destination */
 	return (dest);
 }
-
