e4bb76e862560b230fd3679d21ec03ea9bcb075a
ST-GuY <estebanlvy23@gmail.com>
2025-06-30 14:55:35 +0200

    betty

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,8 @@
  * @s: Chaîne dans laquelle on recherche
  * @accept: Chaîne contenant les octets à rechercher
  *
- * Retourne un pointeur vers la première occurrence d'un octet de accept dans s,
+ * Return un pointeur vers la première occurrence
+ * d'un octet de accept dans s,
  * ou NULL si aucun octet n'est trouvé.
  */
 char *_strpbrk(char *s, char *accept)
@@ -14,7 +15,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++) /* Parcours chaque caractère de s */
 	{
-		for (j = 0; accept[j] != '\0'; j++) /* Parcours chaque caractère de accept */
+		for (j = 0; accept[j] != '\0'; j++) /* Parcours chaque caractère de accept*/
 		{
 			if (s[i] == accept[j]) /* Si le caractère de s est dans accept */
 			{
