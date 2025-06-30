287f8a35fdf8c67859086168d597ed954ff5615b
ST-GuY <estebanlvy23@gmail.com>
2025-06-30 09:57:31 +0200

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * _memset - Remplit une zone mémoire avec une valeur constante
+ * _memset - Remplit un bloc de mémoire avec une valeur constante
  * @s: pointeur vers la zone mémoire à remplir
- * @b: valeur à copier dans la zone mémoire
+ * @b: valeur à utiliser pour remplir (octet constant)
  * @n: nombre d'octets à remplir
  *
- * Return: un pointeur vers la zone mémoire s
+ * Return: pointeur vers la mémoire remplie (s)
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	/* Utilise la fonction memset standard pour remplir s avec b sur n octets */
-	memset(s, b, n);
+    unsigned int i; /* Déclare une variable pour l'index de boucle */
 
-	/* Retourne le pointeur vers la zone mémoire modifiée */
-	return (s);
+    /* Boucle à travers les n octets */
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b; /* Affecte la valeur b à chaque octet */
+    }
+
+    return (s); /* Retourne le pointeur vers la mémoire remplie */
 }
