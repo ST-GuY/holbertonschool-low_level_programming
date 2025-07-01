7ff7f204f9c019575d27c5f2c58ecc182f80f622
ST-GuY <estebanlvy23@gmail.com>
2025-07-01 14:44:55 +0200

    factoriel

---
+++ recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - Calcule la factorielle d'un entier de façon récursive
+ * @n: L'entier dont on veut calculer la factorielle
+ *
+ * Return: La factorielle de n, ou -1 si n est négatif
+ */
+
+int factorial(int n)
+{
+	if (n < 0) /* Vérifie si n est négatif */
+	{
+		return (-1); /* Retourne -1 pour indiquer une erreur */
+	}
+
+	if (n == 0) /* Cas de base : 0! = 1 */
+	{
+		return (1); /* Retourne 1 pour n == 0 */
+	}
+	else /* Cas récursif : n! = n * (n - 1)! */
+	{
+		return (n * factorial(n - 1)); /* Appel récursif */
+	}
+}
