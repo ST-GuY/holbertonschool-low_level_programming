76fe7cf7559d19e39e3a958cf91ceb1d19e9bc8a
ST-GuY <estebanlvy23@gmail.com>
2025-07-02 16:04:13 +0200

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Affiche le nombre d'arguments passés au programme
+ * @argc: Le nombre total d'arguments
+ * @argv: Tableau contenant les arguments (non utilisé ici)
+ *
+ * Return: 0 (succès)
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv; /* On ignore argv, car on n'en a pas besoin */
+
+	/* On soustrait 1 à argc pour ne pas compter le nom du programme */
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
