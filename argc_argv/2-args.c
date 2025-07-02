7775c80ce50ad73ac4779fa90a10571383e95096
ST-GuY <estebanlvy23@gmail.com>
2025-07-02 16:48:16 +0200

    args

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
-/*
- * Programme simple qui affiche tous les arguments passés en ligne de commande.
- * argc : nombre total d'arguments (le nom du programme inclus).
- * argv : tableau des arguments sous forme de chaînes de caractères.
- */
+/**
+* main - Programme simple qui affiche tous les arguments
+* passés en ligne de commande
+* @argc: nombre total d'arguments (le nom du programme inclus).
+* @argv: tableau des arguments sous forme de chaînes de caractères.
+* Return: 0 si le programme s'exécute correctement
+*/
 
 int main(int argc, char **argv)
 {
-	int i;/* Compteur pour parcourir les arguments */
+	int i; /* Déclaration d'un compteur pour parcourir les arguments */
 
-    for (i = 0; i < argc; i++)/* Boucle sur tous les arguments */
+	for (i = 0; i < argc; i++) /* Boucle pour itérer sur tous les arguments */
 	{
-        printf("%s\n", argv[i]);/* Affiche l'argument i suivi d'un saut de ligne */
-    }
-    return 0;
+		printf("%s\n", argv[i]); /*Affiche l'argument courant*/
+	}
+	return (0); /* Fin du programme avec un code de sortie 0 (succès) */
 }
