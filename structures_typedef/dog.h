f4a5e87827d71adebc9efc36431f50ec72cab302
ST-GuY <estebanlvy23@gmail.com>
2025-07-09 16:38:27 +0200

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * struct dog - Structure that defines a dog
+ * @name: Pointer to a string for the dog's name
+ * @age: Float value for the dog's age
+ * @owner: Pointer to a string for the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
