07ef5e2fd94aad232b31583322ed7d3ff942ad21
ST-GuY <estebanlvy23@gmail.com>
2025-07-09 11:05:24 +0200

    betty

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -7,19 +7,19 @@
  * Return: pointeur vers la mémoire allouée ou NULL en cas d'échec
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
-{	
+{
 	char *ptr; /* Pointeur pour parcourir la mémoire allouée */
 	unsigned int i; /* Compteur pour la boucle */
 
 	if (nmemb == 0 || size == 0) /* Vérifie si les paramètres sont valides */
 		return (NULL);
-	
+
 	ptr = malloc(nmemb * size); /* Alloue la mémoire */
 
 	if (ptr == NULL) /* Vérifie si l'allocation a échoué */
 		return (NULL);
 
-	for(i = 0; i < (nmemb * size); i++) /* Initialise chaque octet à 0 */
+	for (i = 0; i < (nmemb * size); i++) /* Initialise chaque octet à 0 */
 		ptr[i] = 0;
 
 	return (ptr);	/* Retourne le pointeur vers la mémoire allouée */
