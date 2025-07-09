f0f3175156a03a7c98a8f4732386e7f741ef662b
ST-GuY <estebanlvy23@gmail.com>
2025-07-09 11:04:10 +0200

    calloc

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -4,8 +4,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
