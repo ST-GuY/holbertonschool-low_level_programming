4bc1e7b370074bc6798e083c520c8efe2fc1478a
ST-GuY <estebanlvy23@gmail.com>
2025-07-09 16:43:38 +0200

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Structure qui décrit un chien
- * @name: Pointeur vers une chaîne de caractères représentant le nom du chien
+ * @name: Pointeur vers une chaîne de caractère représentant le nom du chien
  * @age: Âge du chien (nombre à virgule flottante)
- * @owner: Pointeur vers une chaîne de caractères représentant le propriétaire du chien
+ * @owner: Pointeur vers une chaîne de caractères
+ * représentant le propriétaire du chien
  */
 
 struct dog
@@ -12,3 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+#endif
