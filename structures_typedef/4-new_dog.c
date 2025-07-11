2bf6a92ffec570bca7617277850677e47e19b46b
ST-GuY <estebanlvy23@gmail.com>
2025-07-11 10:39:28 +0200

    free

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -69,9 +69,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->owner = malloc(str_len(owner) + 1);
 	if (d->owner == NULL)
 	{
-		free(d->name);
-		free(d);
-		return (NULL);
+		free(d->name); 	/* libère ce qu’on avait déjà alloué */
+		free(d);		/* libère la structure elle-même */
+		return (NULL);	/* signale l’échec */
 	}
 
 	/* Copie les chaînes name et owner dans la structure */
