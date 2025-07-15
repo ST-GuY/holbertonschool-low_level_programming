84430f63f89e4c1c6937e20477d83200ec3f1a5c
ST-GuY <estebanlvy23@gmail.com>
2025-07-15 10:16:31 +0200

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,11 +4,12 @@
 /**
  * print_name - Applique une fonction à une chaîne de caractères (nom).
  * @name: Le nom à traiter.
- * @f: Pointeur vers une fonction qui prend un char * en paramètre et ne retourne rien.
- *
+ * @f: Pointeur vers une fonction qui prend
+ * un char * en paramètre et ne retourne rien.
  * Description : Cette fonction vérifie que les pointeurs ne sont pas NULL,
  * puis elle appelle la fonction pointée par f en lui passant name.
- * Cela permet d'utiliser différentes fonctions pour afficher ou manipuler le nom.
+ * Cela permet d'utiliser différentes fonctions
+ * pour afficher ou manipuler le nom.
  */
 void print_name(char *name, void (*f)(char *))
 {
