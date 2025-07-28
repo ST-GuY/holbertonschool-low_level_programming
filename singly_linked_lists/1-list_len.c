fc954c8ea7c1684f94cc50e115dd34d422c17de2
ST-GuY <estebanlvy23@gmail.com>
2025-07-28 13:51:22 +0200

    list len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Compte le nombre d'éléments dans une liste chaînée list_t
+ * @h: Pointeur vers le premier élément de la liste
+ *
+ * Return: Le nombre d'éléments dans la liste
+ */
+size_t list_len(const list_t *h) /* Déclaration de la fonction list_len prenant un pointeur vers list_t en paramètre */
+{
+	size_t count = 0; /* Initialise une variable pour compter les éléments de la liste */
+
+	while (h != NULL) /* Boucle tant que le pointeur n'est pas NULL (fin de la liste) */
+	{
+		count++;     /* Incrémente le compteur pour chaque élément */
+		h = h->next; /* Passe à l’élément suivant de la liste */
+	}
+	return (count); /* Retourne le nombre total d’éléments comptés */
+}
