db9744fb966affb4c34d47d2e253ccbc3d04b871
ST-GuY <estebanlvy23@gmail.com>
2025-08-01 15:15:24 +0200

    append text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,7 +10,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
+
+		if (file == -1)
 		return (-1);
 
 		if (text_content == NULL)
