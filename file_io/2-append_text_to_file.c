635b9fc0e4811896e9c258b89e8743c8e53b7e05
ST-GuY <estebanlvy23@gmail.com>
2025-08-01 18:16:51 +0200

    betty

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,9 +3,10 @@
 /**
  * append_text_to_file - Ajoute du texte à la fin d’un fichier.
  * @filename: Nom du fichier dans lequel écrire.
- * @text_content: Chaîne de caractères (terminée par '\0') à ajouter au fichier.
+ * @text_content: Chaîne de caractères
+ * (terminée par '\0') à ajouter au fichier.
  *
- * Retour: 1 en cas de succès, -1 en cas d’échec (fichier introuvable ou non accessible).
+ * Return: 1 en cas de succès, -1 en cas d’échec.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -15,38 +16,28 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	/* Vérifie si le nom du fichier est NULL */
 	if (filename == NULL)
-	{
 		return (-1); /* Échec : nom de fichier non fourni */
-	}
-
 	/* Ouvre le fichier en mode écriture seule + ajout à la fin */
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1); /* Échec : impossible d’ouvrir le fichier */
-
 	/* Si le contenu à écrire est NULL, on ne fait rien et on retourne 1 */
 	if (text_content == NULL)
 	{
 		close(file); /* On ferme le fichier malgré tout */
 		return (1); /* Succès : rien à écrire */
 	}
-
 	/* Calcule la longueur de la chaîne text_content */
 	while (text_content[len] != '\0')
-	{
 		len++;
-	}
-
 	/* Écrit le contenu dans le fichier */
 	written = write(file, text_content, len);
-
 	/* Vérifie que tous les caractères ont bien été écrits */
 	if (written == -1 || (ssize_t)written != len)
 	{
 		close(file); /* Ferme le fichier en cas d'erreur */
 		return (-1); /* Échec de l’écriture */
 	}
-
 	/* Ferme le fichier après écriture */
 	close(file);
 	return (1); /* Succès */
